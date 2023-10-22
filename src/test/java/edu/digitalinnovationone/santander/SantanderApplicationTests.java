e26aab12bc13c5a352c3b2177a1e49a8fc17a47a
riBergamo <rrianebergamo@gmail.com>
2023-10-22 20:19:21 -0300

    first commit

---
+++ src/test/java/edu/digitalinnovationone/santander/SantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.digitalinnovationone.santander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
